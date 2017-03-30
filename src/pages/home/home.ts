e739e21508161f0f314f366871b056a8b4555957
M. Smith <animaniac21@gmail.com>
2017-03-30 18:40:52 -0500

    resolved merge conflicts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,8 @@ import { NavController, Platform } from 'ionic-angular';
 
 import { SignaturePad } from 'angular2-signaturepad/signature-pad';
 
+import { Camera } from 'ionic-native';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -22,8 +24,25 @@ export class HomePage {
     'canvasHeight': this.plt.height()
   };
 
+  private imageSrc:string;
   constructor(public navCtrl: NavController, public plt: Platform) {
-    
+
+  }
+
+  openGallery (): void {
+    let cameraOptions = {
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: Camera.DestinationType.FILE_URI,      
+      quality: 100,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      encodingType: Camera.EncodingType.JPEG,      
+      correctOrientation: true
+    }
+
+    Camera.getPicture(cameraOptions)
+    .then(file_uri => this.imageSrc = file_uri, 
+      err => console.log(err));   
   }
 
   ngAfterViewInit() {
@@ -46,7 +65,6 @@ export class HomePage {
   clearRock() {
     this.signaturePad.clear();
   }
-
   toggleStyleBar(toolStr) {
     if (toolStr == 'text') {
         this.paintStyleShow = false;
